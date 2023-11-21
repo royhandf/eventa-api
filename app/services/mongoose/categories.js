d9733e8c0214708b3c9422ef86bc6156bca1361f
royhandf <royhandaffa14@gmail.com>
2023-11-21 11:26:18 +0700

    feat: create api endpoints for events

--- app/services/mongoose/categories.js
+++ app/services/mongoose/categories.js
@@ -62,10 +62,19 @@ const deleteCategories = async (req) => {
   return result;
 };
 
+const checkingCategories = async (id) => {
+  const result = await Categories.findOne({ _id: id });
+
+  if (!result) throw new NotFoundError(`Tidak ada Kategori dengan id :  ${id}`);
+
+  return result;
+};
+
 module.exports = {
   getAllCategories,
   createCategories,
   getOneCategories,
   updateCategories,
   deleteCategories,
+  checkingCategories,
 };
