2620295416c237164c23f998ec327fe9c3ee5045
royhandf <royhandaffa14@gmail.com>
2023-11-21 11:25:36 +0700

    refactor: remove unnecessary comments

--- app/services/mongoose/talents.js
+++ app/services/mongoose/talents.js
@@ -1,8 +1,6 @@
-// import model Talents
 const Talents = require("../../api/v1/talents/model");
 const { checkingImage } = require("./images");
 
-// import custom error not found dan bad request
 const { NotFoundError, BadRequestError } = require("../../errors");
 
 const getAllTalents = async (req) => {
@@ -27,13 +25,10 @@ const getAllTalents = async (req) => {
 const createTalents = async (req) => {
   const { name, role, image } = req.body;
 
-  // cari image dengan field image
   await checkingImage(image);
 
-  // cari talents dengan field name
   const check = await Talents.findOne({ name });
 
-  // apa bila check true / data talents sudah ada maka kita tampilkan error bad request dengan message pembicara duplikat
   if (check) throw new BadRequestError("pembicara nama duplikat");
 
   const result = await Talents.create({ name, image, role });
@@ -61,16 +56,13 @@ const updateTalents = async (req) => {
   const { id } = req.params;
   const { name, image, role } = req.body;
 
-  // cari image dengan field image
   await checkingImage(image);
 
-  // cari talents dengan field name dan id selain dari yang dikirim dari params
   const check = await Talents.findOne({
     name,
     _id: { $ne: id },
   });
 
-  // apa bila check true / data talents sudah ada maka kita tampilkan error bad request dengan message pembicara nama duplikat
   if (check) throw new BadRequestError("pembicara nama duplikat");
 
   const result = await Talents.findOneAndUpdate(
@@ -79,7 +71,6 @@ const updateTalents = async (req) => {
     { new: true, runValidators: true }
   );
 
-  // jika id result false / null maka akan menampilkan error `Tidak ada pembicara dengan id` yang dikirim client
   if (!result)
     throw new NotFoundError(`Tidak ada pembicara dengan id :  ${id}`);
 
