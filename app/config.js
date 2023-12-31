8ef44774aa2ead95b1ecfa05daaae757248bc8f3
Royhan Daffa <70930874+royhandf@users.noreply.github.com>
2023-12-31 13:23:25 +0700

    refactor: change variable mongodb

--- app/config.js
+++ app/config.js
@@ -2,7 +2,7 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 module.exports = {
-  urlDb: process.env.URL_MONGODB_DEV,
+  urlDb: process.env.URL_MONGODB,
   jwtExpiration: process.env.JWT_EXPIRATION,
   jwtSecret: process.env.JWT_SECRET,
   gmail: process.env.GMAIL,
