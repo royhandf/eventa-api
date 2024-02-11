b9cde33852bef4ecfbced469d963f2ec53465495
royhandf <royhandaffa14@gmail.com>
2024-02-11 20:02:49 +0700

    fix: update value jwt secret key

--- app/config.js
+++ app/config.js
@@ -4,8 +4,8 @@ dotenv.config();
 module.exports = {
   urlDb: process.env.URL_MONGODB,
   jwtExpiration: process.env.JWT_EXPIRATION,
-  jwtSecret: process.env.JWT_SECRET,
-  jwtRefreshTokenSecret: process.env.JWT_REFRESH_TOKEN_SECRET,
+  jwtSecret: process.env.JWT_SECRET_KEY,
+  jwtRefreshTokenSecret: process.env.JWT_REFRESH_TOKEN_SECRET_KEY,
   jwtRefreshTokenExpiration: process.env.JWT_REFRESH_TOKEN_EXPIRATION,
   gmail: process.env.GMAIL,
   password: process.env.PASSWORD,
