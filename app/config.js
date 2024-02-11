9458ca2e65905e8a286a62222b0102912c901e02
royhandf <royhandaffa14@gmail.com>
2024-02-11 19:59:30 +0700

    feat: add refresh token

--- app/config.js
+++ app/config.js
@@ -5,6 +5,8 @@ module.exports = {
   urlDb: process.env.URL_MONGODB,
   jwtExpiration: process.env.JWT_EXPIRATION,
   jwtSecret: process.env.JWT_SECRET,
+  jwtRefreshTokenSecret: process.env.JWT_REFRESH_TOKEN_SECRET,
+  jwtRefreshTokenExpiration: process.env.JWT_REFRESH_TOKEN_EXPIRATION,
   gmail: process.env.GMAIL,
   password: process.env.PASSWORD,
 };
--- app/services/mongoose/auth.js
+++ app/services/mongoose/auth.js
@@ -1,6 +1,7 @@
 const Users = require("../../api/v1/users/model");
 const { BadRequestError, UnauthorizedError } = require("../../errors");
-const { createTokenUser, createJWT } = require("../../utils");
+const { createTokenUser, createJWT, createRefreshJWT } = require("../../utils");
+const { createUserRefreshToken } = require("./refreshToken");
 
 const signin = async (req) => {
   const { email, password } = req.body;
@@ -12,18 +13,25 @@ const signin = async (req) => {
   const result = await Users.findOne({ email: email });
 
   if (!result) {
-    throw new UnauthorizedError("Invalid credentials");
+    throw new UnauthorizedError("Invalid Credentials");
   }
 
   const isPasswordCorrect = await result.comparePassword(password);
 
   if (!isPasswordCorrect) {
-    throw new UnauthorizedError("Invalid credentials");
+    throw new UnauthorizedError("Invalid Credentials");
   }
 
   const token = createJWT({ payload: createTokenUser(result) });
 
-  return { token, role: result.role };
+  const refreshToken = createRefreshJWT({ payload: createTokenUser(result) });
+
+  await createUserRefreshToken({
+    refreshToken,
+    user: result._id,
+  });
+
+  return { token, refreshToken, role: result.role, email: result.email };
 };
 
 module.exports = { signin };
