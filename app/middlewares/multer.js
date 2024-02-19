8bbced3cf826559a90b4a797ff8131e24ccfce7e
royhandf <royhandaffa14@gmail.com>
2024-02-19 11:27:51 +0700

    fix: destination images

--- app/middlewares/multer.js
+++ app/middlewares/multer.js
@@ -2,7 +2,7 @@ const multer = require("multer");
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "public/uploads/");
+    cb(null, "/public/uploads");
   },
   filename: function (req, file, cb) {
     cb(null, Math.floor(Math.random() * 99999999) + "-" + file.originalname);
